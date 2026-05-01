7e42733b948647a1e1a177cfa49300666c27eb33
arthurmwakima <34554151+arthurmwakima@users.noreply.github.com>
2026-04-30 22:45:47 -0700

    Update Navbar.tsx

--- twentyfirst-connect/src/components/Navbar.tsx
+++ twentyfirst-connect/src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import { Menu, X, Wifi } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { motion, AnimatePresence } from "framer-motion";
 import { Link, useLocation } from "react-router-dom";
-import TopHeader from "@/components/TopHeader";
+
 
 const navItems = [
   { label: "Home", href: "/" },
